12a2a0631114462a2ff07d39a777d579ac3716eb
Tasuku Kubo <e175764@ie.u-ryukyu.ac.jp>
2018-11-09 18:25:26 +0900

    ManageApp Project Start

---
+++ ManageApp/NewSubjectSettingViewController.swift
@@ -0,0 +1,44 @@
+//
+//  NewSubjectSettingViewController.swift
+//  ManageApp
+//
+//  Created by Tasuku Kubo on 2018/11/08.
+//  Copyright © 2018 TasukuKubo. All rights reserved.
+//
+
+import UIKit
+
+var Subject = [String]()
+
+class NewSubjectSettingViewController: UIViewController {
+   
+    @IBOutlet weak var TodoTextField: UITextField!
+
+    @IBAction func TodoAddButtom(_ sender: Any) {
+        //変数に入力内容を入れる
+        Subject.append(TodoTextField.text!)
+        //追加ボタンを押したらフィールドを空にする
+        TodoTextField.text = ""
+        //変数の中身をUDに追加
+        UserDefaults.standard.set( Subject, forKey: "TodoList" )
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
---
+++ ManageApp/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  ManageApp
+//
+//  Created by Tasuku Kubo on 2018/11/08.
+//  Copyright © 2018 TasukuKubo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController : UIViewController,UITableViewDelegate,UITableViewDataSource{
+    //UITableView、numberOfRowsInSectionの追加(表示するcell数を決める)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //戻り値の設定(表示するcell数)
+        return Subject.count
+    }
+    
+    
+    //UITableView、cellForRowAtの追加(表示するcellの中身を決める)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //変数を作る
+        let TodoCell :UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        //変数の中身を作る
+        TodoCell.textLabel!.text = Subject[indexPath.row]
+        //戻り値の設定（表示する中身)
+        return TodoCell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let index = indexPath.row
+        Subject.remove(at: index) //ここでエラーが発生しました
+        tableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        if UserDefaults.standard.object(forKey: "TodoList") != nil {
+            Subject = UserDefaults.standard.object(forKey: "TodoList") as! [String]
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
