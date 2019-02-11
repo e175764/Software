9d01ad1de33fcfd0a205d45005bc3c0d4eb7ab68
Tasuku Kubo <e175764@ie.u-ryukyu.ac.jp>
2019-02-11 21:19:27 +0900

    Add editing function

---
+++ ManageApp/EditExerciseViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EditExerciseViewController.swift
+//  ManageApp
+//
+//  Created by Tasuku Kubo on 2019/02/11.
+//  Copyright © 2019 TasukuKubo. All rights reserved.
+//
+
+import Foundation
